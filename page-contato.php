eddfbf96d062dac8063e62faadecdb466e6550b5
Luiz Serão <luiz.serao@hotmail.com>
2024-09-02 08:40:22 -0300

    header/footer e Template Name

---
+++ page-contato.php
@@ -0,0 +1,42 @@
+<?php
+	// Template Name: Contato
+?>
+<?php
+	get_header();
+?>
+		</div>
+	</section>
+	<section class="contato">
+		<div class="center">
+			<div class="w50 contato-info">
+				<h2>A mais importante,<br/>primeira conversa.</h2>
+				<br />
+				<p><b>Telefone:</b> (48) 9901-3620</p>
+				<p><b>Telefone:</b> (48) 9901-3620</p>
+				<p><b>Telefone:</b> (48) 9901-3620</p>
+
+			<div class="mapa-container">
+				<div id="mapa"></div>
+			</div><!--mapa-->
+			</div>
+
+			<div class="w50 contato-form">
+				<form>
+					<input placeholder="Nome" type="text" />
+					<input placeholder="E-mail" type="text" />
+					<input placeholder="Telefone" type="text" />
+					<select>
+						<option>Geral</option>
+						<option>Suporte</option>
+					</select>
+					<textarea placeholder="Mensagem"></textarea>
+					<input type="submit" value="Enviar!">
+				</form>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</section>
+
+<?php
+	get_footer();
+?>
